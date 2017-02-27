c4ff28e39b84f719502ce693ec87a5716f022beb
prestegaard <hakonp@gmail.com>
2017-02-27 14:48:29 +0100

    Made files for unittesting, also added these in makefile

--- rc-car/CODE/DUMMY_FOLDER/rc_car/main.c
+++ rc-car/CODE/DUMMY_FOLDER/rc_car/main.c
@@ -31,8 +31,8 @@
 
 #include "rc_motor.h"
 #include "rc_uart.h"
-
-
+#include "rc_ultrasound.h"
+#include "rc_unit_test.h"
 
 
 
@@ -52,66 +52,27 @@ void clear_led(uint32_t number){
     nrf_gpio_pin_set(number);
 }
 
-void unit_test_motor(){
-    // DISABLE LEFT MOTOR
-    motor_set_speed(LEFT, 0);
-
-    // RIGHT MOTOR FORWARD
-    motor_set_dir(RIGHT, FORWARD);
-    motor_set_speed(RIGHT, 200);
-    motor_start();
-    nrf_delay_ms(1000);
-    motor_set_speed(RIGHT, 500);
-    nrf_delay_ms(1000);
-
-    // RIGHT MOTOR BACKWARD
-    motor_set_dir(RIGHT, BACKWARD);
-    nrf_delay_ms(1000);
-    motor_set_speed(RIGHT, 200);
-    nrf_delay_ms(1000);
-
-    // PAUSE
-    motor_stop();
-    nrf_delay_ms(1000);
-
-    // DISABLE RIGHT MOTOR
-    motor_set_speed(RIGHT, 0);
-
-    // LEFT MOTOR FORWARD 
-    motor_set_dir(LEFT, FORWARD);
-    motor_set_speed(LEFT, 200);
-    motor_start();
-    nrf_delay_ms(1000);
-    motor_set_speed(LEFT, 500);
-    nrf_delay_ms(1000);
-
-    // LEFT MOTOR BACKWARD
-    motor_set_dir(LEFT, BACKWARD);
-    nrf_delay_ms(1000);     
-    motor_set_speed(LEFT, 200);
-    nrf_delay_ms(1000);
-    motor_stop();
-}
 
 int main(void)
 {
     leds_init();
     uart_init();   
     motor_init();
-
+    ultrasound_init();
     printf("\r\nEiT Lastebil: \r\n");
 
     motor_set_speed(RIGHT, 500);    
     motor_start();
-   // unit_test_motor();
-    uint32_t i = 0;
+
+
+    unit_test_motor();
+    unit_test_ultrasound();
     for (;;){
     //    uint8_t cr;
     //    while (app_uart_get(&cr) != NRF_SUCCESS);
     //    while (app_uart_put(cr) != NRF_SUCCESS);
         nrf_delay_ms(1000);
-        printf("i = %d \r\n", i);
-        i++;
+        //printf("Distance: %d mm\t \r\n", ultrasound_get_distance());
       }
 }
 
